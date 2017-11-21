b5959d163eb47c2addea9c5dfe672b81414e584c
Dropsu <stube01@gmail.com>
2017-11-21 23:06:48 +0100

    connection with db working

--- PizzeriaSconosciuto/MainWindow.xaml.cs
+++ PizzeriaSconosciuto/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,48 @@ namespace PizzeriaSconosciuto
     {
         public MainWindow()
         {
+            // NA RAZIE KOD BAZY DANYCH TUTAJ BO NIE WIEM GDZIE DO DAC, POTEM ZREFAKTORYZUJEMU ;)
+            SqlConnection conn = new SqlConnection(
+            "Data Source=.\\SQLEXPRESS;Initial Catalog=Pizzeria;User ID=Pizza;Password=pizza");
+
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                // 3. Pass the connection to a command object
+                SqlCommand cmd = new SqlCommand("select * from Dostawy", conn);
+
+                //
+                // 4. Use the connection
+                //
+
+                // get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CustomerID of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // 5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
             InitializeComponent();
         }
     }
