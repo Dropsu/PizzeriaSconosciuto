53123d924343a6b75529412c52df543733aa04bd
Dropsu <stube01@gmail.com>
2017-11-23 17:16:34 +0100

    Adding, updating, deleting, listing

--- PizzeriaSconosciuto/MainWindow.xaml.cs
+++ PizzeriaSconosciuto/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
@@ -23,6 +24,7 @@ namespace PizzeriaSconosciuto
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSQLDataContext db;
         public MainWindow()
         {
             // NA RAZIE KOD BAZY DANYCH TUTAJ BO NIE WIEM GDZIE DO DAC, POTEM ZREFAKTORYZUJEMU ;)
@@ -31,7 +33,7 @@ namespace PizzeriaSconosciuto
 
             SqlConnection conn = new SqlConnection(
             "Data Source = .\\SQLEXPRESS; Initial Catalog=Pizzeria;User ID=Pizzus;Password=pizza");
-            LinqToSQLDataContext db = new LinqToSQLDataContext(conn);
+            db = new LinqToSQLDataContext(conn);
 
 
 
@@ -44,6 +46,8 @@ namespace PizzeriaSconosciuto
 
 
             InitializeComponent();
+
+            Loaded += Window_Loaded;
         }
 
         public void dodajProdukt(LinqToSQLDataContext db, string nazwa, int Ilosc, double cena)
@@ -60,12 +64,65 @@ namespace PizzeriaSconosciuto
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            dodajProdukt(db, nazwa_box.Text, Int32.Parse(ilosc_box.Text), Int32.Parse(cena_box.Text));
+            fillTablie();
         }
 
         private void button_Click_1(object sender, RoutedEventArgs e)
         {
+            usunProdukt(Int32.Parse(produktid_box.Text));
+            fillTablie();
+        }
 
+        private void usunProdukt(int id)
+        {
+            var delete =
+            from Produkty in db.Produkties
+            where Produkty.IdProduktu == id
+            select Produkty;
+
+            foreach (var produkt in delete)
+            {
+                db.Produkties.DeleteOnSubmit(produkt);
+            }
+
+            try
+            {
+                db.SubmitChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                // Provide for exceptions.
+            }
+        }
+
+        private void fillTablie()
+        {
+            Table<Produkty> Produkty = db.GetTable<Produkty>();
+            var q =
+               from p in Produkty
+               select p;
+            dataGrid.ItemsSource = q.ToList();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            fillTablie();
+        }
+
+        private void zmien_Click(object sender, RoutedEventArgs e)
+        {
+
+            Produkty result = (from p in db.Produkties
+                               where p.IdProduktu == Int32.Parse(produktid_box.Text)
+                               select p).SingleOrDefault();
+
+            result.Ilosc = Int32.Parse(ilosc_box.Text);
+            result.Nazwa = nazwa_box.Text;
+            result.cena = Int32.Parse(cena_box.Text);
+
+            db.SubmitChanges();
         }
     }
 
